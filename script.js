732f4a3a608eac4facf2804a010153b9d2c19242
robinucar <mehmet.ucar@imediapro.co.uk>
2021-08-05 14:28:14 +0300

    test1, question 1

---
+++ script.js
@@ -0,0 +1,23 @@
+const testArray = [
+  {
+    name: 'John',
+    id: 1,
+  },
+  {
+    name: 'Mary',
+    id: 2,
+  },
+  {
+    name: 'Terry',
+    id: 3,
+  },
+].map((e) => e.id);
+
+console.log(testArray);
+
+//in the console we will see the ids of elements.
+/**
+ * 0: 1
+ * 1: 2
+ * 2: 3
+ */
